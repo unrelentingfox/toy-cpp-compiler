d90c7612d87c3768fc19db30dc96d5dd2f296eeb
Dustin <dustin.o.fox@gmail.com>
2017-12-07 00:36:35 -0800

    updated icodegen to include TAC operations from tac.h and tac.c in lecture.

--- header/icodegen.h
+++ header/icodegen.h
@@ -6,30 +6,57 @@
 
 #include "symtab.h"
 
-enum TACOperationType {
-  NEG,
-  ADD,
-  SUB,
-  MUL,
-  DIV,
-  ASN,
-  ADDR,
-  LCOUNT,
-  SCOUNT,
-  GOTO,
-  BLT,
-  BIF,
-  BNIF,
-  PARM,
-  CALL,
-  RET
+enum MemoryRegion {
+  GLOBAL_R = 2001,
+  LOCAL_R = 2002,
+  PARAM_R = 2003,
+  CLASS_R = 2004,
+  LABEL_R = 2005,
+  CONST_R = 2006,
 };
+
+enum TACOperationType {
+  /* Opcodes, per lecture notes */
+  ADD_O   = 3001,
+  SUB_O   = 3002,
+  MUL_O   = 3003,
+  DIV_O   = 3004,
+  NEG_O   = 3005,
+  ASN_O   = 3006,
+  ADDR_O  = 3007,
+  LCONT_O = 3008,
+  SCONT_O = 3009,
+  GOTO_O  = 3010,
+  BLT_O   = 3011,
+  BLE_O   = 3012,
+  BGT_O   = 3013,
+  BGE_O   = 3014,
+  BEQ_O   = 3015,
+  BNE_O   = 3016,
+  BIF_O   = 3017,
+  BNIF_O  = 3018,
+  PARM_O  = 3019,
+  CALL_O  = 3020,
+  RET_O   = 3021,
+  /* declarations/pseudo instructions */
+  GLOB_D  = 3051,
+  PROC_D  = 3052,
+  LOCAL_D = 3053,
+  LABEL_D = 3054,
+  END_D   = 3055
+};
+
+typedef struct MemoryAddress {
+  enum MemoryRegion region;
+  int offset;
+} Address;
 
 struct TACInstruction {
   enum TACOperationType op;
-  int a1;
-  int a2;
-  int a3;
+  struct MemoryAddress a1;
+  struct MemoryAddress a2;
+  struct MemoryAddress a3;
+  TACInstruction *next;
 };
 
 struct TACList {
@@ -42,27 +69,11 @@ struct TACList {
  * Operations for Three Address Code Data types.
  * All TAC functions start with TAC_ for clarity.
  */
-struct TACInstruction *TAC_new_neg(int a1, int a2);
-struct TACInstruction *TAC_new_add(int a1, int a2, int a3);
-struct TACInstruction *TAC_new_sub(int a1, int a2, int a3);
-struct TACInstruction *TAC_new_mul(int a1, int a2, int a3);
-struct TACInstruction *TAC_new_div(int a1, int a2, int a3);
-struct TACInstruction *TAC_new_asn(int a1, int a2);
-struct TACInstruction *TAC_new_addr(int a1, int a2);
-struct TACInstruction *TAC_new_lcount(int a1, int a2);
-struct TACInstruction *TAC_new_scount(int a1, int a2);
-struct TACInstruction *TAC_new_goto(int a1, int a2);
-struct TACInstruction *TAC_new_blt(int a1, int a2);
-struct TACInstruction *TAC_new_bif(int a1, int a2);
-struct TACInstruction *TAC_new_bnif(int a1, int a2);
-struct TACInstruction *TAC_new_param(int a1, int a2);
-struct TACInstruction *TAC_new_call(int a1, int a2, int a3);
-struct TACInstruction *TAC_new_ret(int a1, int a2);
+struct TACInstruction *TAC_new_instr(enum TACOperationType op, struct MemoryAddress *a1, struct MemoryAddress *a2, struct MemoryAddress *a3);
+struct TACList *TAC_new_list(struct TACInstruction *instr);
 
-
-icode_new_temp(Type *type);
-icode_new_label();
-icode_new_instr(enum TACOperationType op,)
-int ic_calc_mem_address(Sytab *symtab);
+void TAC_concat_list(struct TACList *a, struct TACList *b);
+void TAC_append_list(struct TACList *list, struct TACInstruction *instr);
+void TAC_prepend_list(struct TACList *list, struct TACInstruction *instr);
 
 #endif
--- source/icodegen.c
+++ source/icodegen.c
@@ -2,5 +2,94 @@
  * Intermediate code generation source file.
  */
 #include "../header/icodegen.h"
+#include "../header/logger.h"
 #include "type.h"
 
+/**
+ * @brief      Create a new TACInstruction.
+ *
+ * @param[in]  op    The TACOperationType
+ * @param      a1    First MemoryAddress
+ * @param      a2    Second MemoryAddress
+ * @param      a3    Third MemoryAddress
+ *
+ * @return     A pointer to the newly created TACInstruction.
+ */
+struct TACInstruction *TAC_new_instr(enum TACOperationType op, struct MemoryAddress *a1, struct MemoryAddress *a2, struct MemoryAddress *a3) {
+  LOG_ASSERT(a1);
+  LOG_ASSERT(a2);
+  LOG_ASSERT(a3);
+  struct TACInstruction *instr = malloc(sizeof(struct TACInstruction));
+  if (instr == NULL) {
+    log_error(INTERNAL_ERROR, "Out of memory while generating Three Address Instruction.\n");
+    exit(INTERNAL_ERROR);
+  }
+  instr->op = op;
+  instr->a1 = a1;
+  instr->a2 = a2;
+  instr->a3 = a3;
+  instr->next = NULL;
+  return instr;
+}
+
+/**
+ * @brief      Create a new TACList.
+ *
+ * @param      instr  The TACInstruction that the list contains.
+ *
+ * @return     A pointer to the newly created TACList.
+ */
+struct TACList *TAC_new_list(struct TACInstruction *instr) {
+  LOG_ASSERT(instr);
+  struct TACList *list = malloc(sizeof(struct TACList));
+  if (list == NULL) {
+    log_error(INTERNAL_ERROR, "Out of memory while generating Three Address List.\n");
+    exit(INTERNAL_ERROR);
+  }
+  list->head = instr;
+  list->tail = instr;
+  list->count = 1;
+  return list;
+}
+
+/**
+ * @brief      Concatinate TACList a and TACList b the result is stored in TACList a.
+ *
+ * @param      a     TACList a the destination list
+ * @param      b     TACList b the list to append
+ */
+void TAC_concat(struct TACList *a, struct TACList *b) {
+  LOG_ASSERT(a);
+  LOG_ASSERT(b);
+  a->tail->next = b->head;
+  a->tail = b->tail;
+  a->count += b->count;
+}
+
+/**
+ * @brief      Append a TACInstruction onto the tail of a TACList.
+ *
+ * @param      list   The TACList
+ * @param      instr  The TACInstruction
+ */
+void TAC_append(struct TACList *list, struct TACInstruction *instr) {
+  LOG_ASSERT(list);
+  LOG_ASSERT(instr);
+  list->tail->next = instr;
+  list->tail = instr;
+  list->count++;
+}
+
+/**
+ * @brief      Prepend a TACInstruction to the head of a TACList.
+ *
+ * @param      list   The TACList
+ * @param      instr  The TACInstruction
+ */
+void TAC_prepend(struct TACList *list, struct TACInstruction *instr) {
+  LOG_ASSERT(list);
+  LOG_ASSERT(instr);
+  instr->next = list->head;
+  list->head = instr;
+  list->count++;
+}
